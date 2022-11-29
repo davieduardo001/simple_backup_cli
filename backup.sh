1fec9e63d18cc0a11c6015a76edb7e212fbee9c5
davieduardo001 <ed.miranda@tutanota.com>
2022-11-29 17:49:41 -0300

    adding the cancellation option

--- backup.sh
+++ backup.sh
@@ -15,8 +15,6 @@ case "$1" in
 	;;
 
 	*)
-		zip -r "$1" "$2"
-		mv "$1.zip" "$3"
 
 		echo \
 		"
@@ -25,6 +23,25 @@ case "$1" in
 			*the dir. 'u want to backup: $2
 			*the dir. where you'll send the backup: $3
 		"
-		read -p "You are shure of that?"
+		read -p "You are shure of that? [Y/n] " response
+		if [ "$response" = "" ]; then
+			echo "Starting backup..."
+			sleep 2s
+		else
+			if [ "$response" = "n" ] || [ "$response" = "N" ]; then
+				echo "Canceling the operation..."
+				sleep 2s
+				exit 0
+			elif [ "$response" = "y" ] || [ "$response" = "Y" ]; then
+				echo "Starting backup..."
+				sleep 2s
+			else
+				echo "command not found..."
+				exit 1
+			fi
+		fi
+
+		zip -r "$1" "$2"
+		mv "$1.zip" "$3"
 	;;
 esac
